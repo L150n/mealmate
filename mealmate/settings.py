7bf32553028c498ec236bbd5b7a5a5021c9bcaf3
Lison <93926601+L150n@users.noreply.github.com>
2023-05-26 15:29:42 +0000

    Pending changes exported from your codespace

--- mealmate/settings.py
+++ mealmate/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b_$e*apui3$^me_3g#dk-t45a&#^(^%sc=i=!maf5+o!f!w)wg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
